3cfd0915ea4062b56259aa2cbc14a23363d0df30
buser <buserqub@mail.ru>
2025-08-19 23:48:46 +0500

    Начал заполнять конфигурацию Dictionary

--- Terminology.App.Infra.Contracts/EF/EntityConfiguration/DictionaryConfiguration.cs
+++ Terminology.App.Infra.Contracts/EF/EntityConfiguration/DictionaryConfiguration.cs
@@ -1,7 +1,15 @@
-﻿namespace Terminology.App.Infra.Contracts.EF.EntityConfiguration
-{
-    internal class DictionaryConfiguration
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Terminology.Domain;
 
+namespace Terminology.App.Infra.Contracts.EF.EntityConfiguration
+{
+    internal class DictionaryConfiguration : IEntityTypeConfiguration<Dictionary>
+    {
+        public void Configure(EntityTypeBuilder<Dictionary> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Has
+        }
     }
 }
